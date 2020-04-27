5fddccc74b4f5c5cf405966d9c17fa6aa24329f7
nekomakes <64197982+nekomakes@users.noreply.github.com>
2020-04-27 10:28:27 -0400

    Update dataquery.py
    Added additional received modem parameters.
    Removed ?_= timestamp from URL on line 19.

--- dataquery.py
+++ dataquery.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3 
 # 
 # Pull current network data from Hitron CGN3 and CODA-4582
-# Also called Rogers Rocket CGN3 Modem, or Rogers Ignite Modem
-# Tested with CODA-4582 Hardware: A1
-#                       Software: 2.0.10.36T6
+# Rogers Rocket CGN3 Modem, or Rogers Ignite Modem
 #
 # https://nko.gg/
 # https://github.com/neko/
-
 
 import requests
 
@@ -19,11 +16,21 @@ data = {
 
 with requests.Session() as se:
     se.post('http://192.168.0.1/goform/login', data=data)
-    recv = se.get('http://192.168.0.1/data/getSysInfo.asp?_=1587633912455')
+    recv = se.get('http://192.168.0.1/data/getSysInfo.asp')
     sysInfo = recv.json()
-    print(sysInfo[0]['WRecPkt'])
-    print(sysInfo[0]['WSendPkt'])
+    print(sysInfo[0]['hwVersion'])
+    print(sysInfo[0]['swVersion'])
+    print(sysInfo[0]['serialNumber'])
+    print(sysInfo[0]['rfMac'])
     print(sysInfo[0]['priDNS'])
     print(sysInfo[0]['secDNS'])
+    print(sysInfo[0]['systemLanUptime'])
     print(sysInfo[0]['systemWanUptime'])
-    print(sysInfo[0]['wanIp'])
+    print(sysInfo[0]['systemTime'])
+    print(sysInfo[0]['timezone'])
+    print(sysInfo[0]['WRecPkt'])
+    print(sysInfo[0]['WSendPkt'])
+    print(sysInfo[0]['lanIp'])
+    print(sysInfo[0]['LRecPkt'])
+    print(sysInfo[0]['LSendPkt'])
+    print(sysInfo[0]['routerMode'])
